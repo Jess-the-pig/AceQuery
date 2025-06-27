36dd1f4ee657932962ccc5aa63346b16b5f80c7a
Jess-the-pig <135271280+Jess-the-pig@users.noreply.github.com>
2025-06-27 20:48:54 +0200

    Getting rid of surplus

--- src/main/java/henrotaym/env/enums/DiseaseName.java
+++
@@ -1,7 +0,0 @@
-package henrotaym.env.enums;
-
-public enum DiseaseName {
-    PARASITE,
-    FUNGI,
-    VIRAL
-}
--- src/main/java/henrotaym/env/enums/StatusName.java
+++
@@ -1,7 +0,0 @@
-package henrotaym.env.enums;
-
-public enum StatusName {
-    ALIVE,
-    DEAD,
-    SICK
-}
